5957bc31f0cdb1d8e7de73c41500fb9b867a670f
Julien BY <156668471+julienby@users.noreply.github.com>
2024-11-25 17:28:00 +0100

    Update example1.py

--- example1.py
+++ example1.py
@@ -2,11 +2,11 @@ from mechareader import Config
 
 # Exemple d'utilisation
 # Charger la configuration
-icaging = Config("config.json")
+mydevice = Config("config.json")
 
 # Exemple d'accès
-print(icaging.node.specs.software.name)  # Devrait afficher "NodeControllerClient"
-print(icaging.node.id)  # Devrait afficher "hostname_TERRAIN001"
-print(icaging.node.communication.ping.cmd)  # Devrait afficher "curl https://status.server.com/check"
-print(icaging.node.host)  # Devrait afficher "curl https://status.server.com/check"
-print(icaging.node.specs.software.long_name)
+print(mydevice.node.specs.software.name)  # Devrait afficher "NodeControllerClient"
+print(mydevice.node.id)  # Devrait afficher "hostname_TERRAIN001"
+print(mydevice.node.communication.ping.cmd)  # Devrait afficher "curl https://status.server.com/check"
+print(mydevice.node.host)  # Devrait afficher "curl https://status.server.com/check"
+print(mydevice.node.specs.software.long_name)
